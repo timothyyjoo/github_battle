be3431b8f3c5601fb0a4cb2c5f6e576826f92877
Timothy Joo <timothy.joo@chargify.com>
2018-09-12 10:24:00 -0500

    Finish building out UI/UX, need AJAX Requests

--- app/components/App.js
+++ app/components/App.js
@@ -3,7 +3,12 @@ var Popular = require('./Popular')
 var ReactRouter = require('react-router-dom')
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
-var Nav = require('./Nav')
+var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle= require('./Battle')
+var Switch = ReactRouter.Switch;
+
+
 
 
 class App extends React.Component {
@@ -12,8 +17,14 @@ class App extends React.Component {
       <Router>
         <div className = "container">
           <Nav />
-          <Route path="/popular" component={Popular} />
-
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path="/popular" component={Popular} />
+            <Route exact path="/battle" component={Battle} />
+            <Route render={function () {
+              return <p> Not Found </p>
+            }}/>
+          </Switch>
         </div>
       </Router>
     )
